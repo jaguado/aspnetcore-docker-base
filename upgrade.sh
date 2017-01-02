bf46a0dcc2d5de9851a25febadf9008d3c1287da
Jorge <jorge@jamsoluciones.cl>
2017-01-02 19:06:02 -0300

    upgrade script added

---
+++ upgrade.sh
@@ -0,0 +1,6 @@
+﻿docker rm -f $(docker ps -a -q)
+docker rmi -f $(docker images -q) 
+git clone https://github.com/jaguado/aspnetcore-docker-base.git
+cd aspnetcore-docker-base
+docker build -t aspnetcore:version1.1 .
+docker run -d -p 8080:5000 -t aspnetcore:version1.1 
